149c15a9c478bd3c0b3866af8e7f7f2d6f0bf096
Миронов Михаил <mironov.ma@phystech.edu>
2026-09-22 15:07:49 +0300

    10

--- get-led/double_push.py
+++ get-led/double_push.py
@@ -5,7 +5,7 @@ def dec2bin(v):
     return list(map(int, bin(v)[2::]))
 
 GPIO.setmode(GPIO.BCM)
-leds = [24, 22, 23, 27, 17, 25, 12, 16]
+leds = [24, 22, 23, 27, 17, 25, 12, 16][::-1]
 GPIO.setup(leds, GPIO.OUT)
 GPIO.output(leds, 0)
 button1 = 9
